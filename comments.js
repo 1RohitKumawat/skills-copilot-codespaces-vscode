695d6d5fc96862048bc7d777eaf8c1ec5c1e0faf
1RohitKumawat <106857718+1RohitKumawat@users.noreply.github.com>
2025-02-26 12:11:06 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,41 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+// create a server
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var comments = [];
+var server = http.createServer(function(req, res) {
+    var parseUrl = url.parse(req.url, true);
+    var pathname = parseUrl.pathname;
+    if(pathname === '/') {
+        fs.readFile('./index.html', function(err, data) {
+            if(err) {
+                console.log(err);
+                res.end('404 Not Found');
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data);
+            }
+        });
+    } else if(pathname === '/submit') {
+        var comment = parseUrl.query;
+        comments.push(comment);
+        res.end('submit success');
+    } else if(pathname === '/getComments') {
+        var str = JSON.stringify(comments);
+        res.end(str);
+    } else {
+        var filepath = path.join(__dirname, pathname);
+        fs.readFile(filepath, function(err, data) {
+            if(err) {
+                console.log(err);
+                res.end('404 Not Found');
+            } else {
+                res.end(data);
+            }
+        });
+    }
 });
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(3000, function() {
+    console.log('listening on 3000');
 });

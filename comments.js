5bb88b9b5d3580de70250158a7e4cac5a9f612b8
1RohitKumawat <106857718+1RohitKumawat@users.noreply.github.com>
2025-02-26 12:20:24 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,63 +1,37 @@
-// create a server
-const http = require('http');
-const fs = require('fs');
-const qs = require('querystring');
-const url = require('url');
+// Create web server with express
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const { method, url: reqUrl } = req;
+app.use(bodyParser.json());
 
-  if (method === 'GET') {
-    if (reqUrl === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      fs.createReadStream('index.html').pipe(res);
-    } else if (reqUrl === '/comments') {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      fs.createReadStream('comments.json').pipe(res);
-    } else {
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.end('404 Not Found');
+app.get("/comments", (req, res) => {
+  fs.readFile("comments.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading comments.json");
+      return;
     }
-  } else if (method === 'POST') {
-    if (reqUrl === '/comments') {
-      let body = '';
-      req.on('data', (chunk) => {
-        body += chunk.toString();
-      });
-      req.on('end', () => {
-        const { comment } = qs.parse(body);
-        fs.readFile('comments.json', (err, data) => {
-          if (err) {
-            res.writeHead(500, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ error: 'Server error' }));
-            return;
-          }
-
-          const comments = JSON.parse(data);
-          comments.push(comment);
-
-          fs.writeFile('comments.json', JSON.stringify(comments), (err) => {
-            if (err) {
-              res.writeHead(500, { 'Content-Type': 'application/json' });
-              res.end(JSON.stringify({ error: 'Server error' }));
-              return;
-            }
-
-            res.writeHead(201, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'Comment added' }));
-          });
-        });
-      });
-    } else {
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.end('404 Not Found');
-    }
-  } else {
-    res.writeHead(405, { 'Content-Type': 'text/html' });
-    res.end('405 Method Not Allowed');
-  }
+    res.send(data);
+  });
 });
 
-server.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.post("/comments", (req, res) => {
+  fs.readFile("comments.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading comments.json");
+      return;
+    }
+    const comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile("comments.json", JSON.stringify(comments), (err) => {
+      if (err) {
+        res.status(500).send("Error writing comments.json");
+        return;
+      }
+      res.send("Comment added");
+    });
+  });
 });
+
+app.listen(3000);
